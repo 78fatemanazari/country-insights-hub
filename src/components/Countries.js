7c70b9687e11f7c327decf61cb1cdc33faefa824
fatemanazari <fatemanazari78@gmail.com>
2023-09-25 11:48:10 +0330

    Add file for countries that will have been fetched from the API

---
+++ src/components/Countries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Countries.css';
+
+function Countries() {
+  return (
+    <div className="countries-container">
+      This is part of countries
+    </div>
+  );
+}
+
+export default Countries;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Sliders from './Sliders';
+import Countries from './Countries';
 
 function Home() {
   return (
     <div className="home">
       <Navbar />
       <Sliders />
-      <div className="countries-container">
-        <p>This is countries part</p>
-      </div>
+      <Countries />
       <footer className="footer">
         <p>This is footer part</p>
       </footer>

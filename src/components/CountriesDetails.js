79d43f911f8d9106299226ee286148193c8f1d3a
fatemanazari <fatemanazari78@gmail.com>
2023-09-25 11:59:49 +0330

    Add details page

---
+++ src/components/CountriesDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/CountriesDetails';
+
+function CountriesDetails(){
+    return(
+        <div className="details-container">
+            <p>This is details page</p>
+        </div>
+    )
+}
+
+export default CountriesDetails;

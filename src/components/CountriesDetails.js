7eecd38e9a08ef2a07117022154a365d2273ad24
fatemanazari <fatemanazari78@gmail.com>
2023-09-25 12:07:35 +0330

    Fix errors

--- src/components/CountriesDetails.js
+++ src/components/CountriesDetails.js
@@ -1,12 +1,12 @@
-import React from "react";
-import '../styles/CountriesDetails';
+import React from 'react';
+import '../styles/CountriesDetails.css';
 
-function CountriesDetails(){
-    return(
-        <div className="details-container">
-            <p>This is details page</p>
-        </div>
-    )
+function CountriesDetails() {
+  return (
+    <div className="details-container">
+      <p>This is details page</p>
+    </div>
+  );
 }
 
 export default CountriesDetails;

4ef9d94583eb7f49bee65a9c809cdcda1d62c098
fatemanazari <fatemanazari78@gmail.com>
2023-09-25 09:30:57 +0330

    Add home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="home">
+            <div className="header">
+                <p>This is header part</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;

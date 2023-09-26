8739389ecb4a7559c0f788dbd9c4011931caeed8
fatemanazari <fatemanazari78@gmail.com>
2023-09-26 10:43:19 +0330

    Navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/Navbar.css';
-import BackIcon from '../assets/back.png';
 import Mic from '../assets/mic.png';
 import Setting from '../assets/setting.png';
 
@@ -8,15 +7,14 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="left">
-        <img className="logo" src={BackIcon} width="15" height="20" alt="logo" />
-        <p className="year">2015</p>
+        <button className="year-btn" type="button">2015</button>
       </div>
       <div className="middle">
-        <p className="year">Most Views</p>
+        <p className="title">Most Views</p>
       </div>
       <div className="right">
-        <img className="mic" src={Mic} width="30" height="30" alt="mic" />
-        <img className="setting" src={Setting} width="35" height="35" alt="setting icon" />
+        <img className="mic" src={Mic} width="20" height="20" alt="mic" />
+        <img className="setting" src={Setting} width="30" height="30" alt="setting icon" />
       </div>
     </div>
   );

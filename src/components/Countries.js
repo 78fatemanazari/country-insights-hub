186cc9d57b0b1ce75227aef66cb426ec28348c3f
fatemanazari <fatemanazari78@gmail.com>
2023-09-27 23:33:35 +0330

    Finish the style

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
 import { fetchCountries } from '../redux/CountriesSlice';
+import goIcon from '../assets/images.png';
 import '../styles/Countries.css';
 
 function Countries() {
@@ -21,7 +22,9 @@ function Countries() {
       || acountry.name === 'Spain'
       || acountry.name === 'Belgium'
       || acountry.name === 'China'
-      || acountry.name === 'Japan',
+      || acountry.name === 'Japan'
+      || acountry.name === 'Iran'
+      || acountry.name === 'Colombia',
   );
   console.log(filter);
   return (
@@ -33,13 +36,20 @@ function Countries() {
               <div>
                 <img src={country.flags} alt={country.alt} className="flags" width="300" height="200" />
               </div>
-              <div>
+              <div className="texts">
                 <p className="countries-name">{country.name}</p>
-                <p className="continent">
-                  continent:
-                  {country.continent}
-                </p>
+                <div className="properties">
+                  <p className="f-title">continent:</p>
+                  <p className="continent">
+                    {country.continent}
+                  </p>
+                  <p className="f-title">Population:</p>
+                  <p className="continent">
+                    {country.population / 100000}
+                  </p>
+                </div>
               </div>
+              <img className="go-icon" src={goIcon} alt="go icon" width="50" height="50" />
             </div>
           ),
         )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Logo from '../assets/logo.png';
 import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">
       <nav className="nav">
-        <h1>Country Insights Hub</h1>
-        <img src={Logo} width="200" height="150" alt="logo" />
+        <div className="nav-items">
+          <button type="button">Back</button>
+          <h1>Country Insights Hub</h1>
+        </div>
+        <div className="nav-links">
+          <a href="/">Home</a>
+          <a href="/">Details</a>
+        </div>
       </nav>
     </div>
   );

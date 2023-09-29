32b318d1bf0dcec3676397308fd1e3bbc83338b1
fatemanazari <fatemanazari78@gmail.com>
2023-09-29 13:17:40 +0330

    Edit Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import DetailsPage from './components/DetailsPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Details" element={<DetailsPage />} />
